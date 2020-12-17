8ebaf88960b39560ce05b1aaad7211b8927cbe3e
zhuichao001 <zhuichao001@163.com>
2020-12-17 19:04:42 +0800

    增加命令统计

--- generator/yaml/redis/status.sh
+++ generator/yaml/redis/status.sh
@@ -1,1 +1,8 @@
 find . | grep yaml | xargs cat | awk '{print $2}' | sort | uniq | wc -l
+
+find . | grep yaml | xargs cat | awk '{print $2}' | sort | uniq >.a
+cat redis.cmds | awk '{print $1":"}' | grep -v -E '^$|//' | sort | uniq >.b
+
+comm -13 .a .b
+
+rm -f .a .b
