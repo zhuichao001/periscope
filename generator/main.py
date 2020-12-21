f2dc604211cf75e4ca08f5a5da5c75449a6d38f9
zhuichao001 <zhuichao001@163.com>
2020-12-21 09:53:41 +0800

    配置化

--- generator/main.py
+++ generator/main.py
@@ -7,16 +7,24 @@ import string
 import option
 import factory
 import transport
+import yaml
 
+
+def getconf():
+    try:
+        with open('./config.yaml') as f:
+            return yaml.safe_load(f)
+    except: 
+        os.exit(-1)
 
 def main():
-    opt = option.Option(num_batch=128, num_operation=16, keylen=8, vallen=32, maxduration=60)
+    conf = getconf()
+    opt = option.Option(conf['num_batch'], conf['num_operation'], conf['keylen'], conf['vallen'], conf['maxduration'])
     while True:
         fac = factory.Factory(opt)
         trans = transport.Transport()
         cmds = fac.produce()
         trans.deliver(cmds)
-
 
 if __name__ == '__main__':
     main()
