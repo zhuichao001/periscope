d3a40585fe6895e7afccdb6925d944aa5bffb53c
zhuichao001 <zhuichao001@163.com>
2020-12-23 11:54:28 +0800

    fix bug

--- generator/formatter.py
+++ generator/formatter.py
@@ -9,18 +9,25 @@ def fmt_string(tmpl, key, **kwargs):
 def fmt_mstring(tmpl, **kwargs):
     if 'keys' in kwargs:
         tmpl = tmpl.replace('({key})+', ' '.join(kwargs['keys']))
+        del kwargs['keys']
     if 'vals' in kwargs:
         tmpl = tmpl.replace('({val})+', ' '.join(kwargs['vals']))
+        del kwargs['vals']
     if 'fields' in kwargs:
         tmpl = tmpl.replace('({field})+', ' '.join(kwargs['fields']))
+        del kwargs['fields']
     if 'kvs' in kwargs:
         tmpl = tmpl.replace('({key} {val})+', util.flat_dict(kwargs['kvs']))
+        del kwargs['kvs']
     if 'fvs' in kwargs:
         tmpl = tmpl.replace('({field} {val})+', util.flat_dict(kwargs['fvs']))
+        del kwargs['fvs']
     if 'sms' in kwargs:
         tmpl = tmpl.replace('({score} {member})+', kwargs['sms'])
+        del kwargs['sms']
     if 'members' in kwargs:
         tmpl = tmpl.replace('({member})+', ' '.join(kwargs['members']))
+        del kwargs['members']
     for var in kwargs:
         tmpl = tmpl.replace('{'+str(var)+'}', str(kwargs[var]))
     return tmpl
--- generator/redis/mhash.py
+++ generator/redis/mhash.py
@@ -17,15 +17,15 @@ class MHash(basetype.BaseType):
         for _ in range(1, 10):
             self.fvs[util.RAND(20)] = util.RAND(30)
         tmpl = super().create()
+        print("|||MHASH fvs:", self.fvs)
         cmd = formatter.fmt_mstring(tmpl, key=self.key, fvs=self.fvs, timeout=self.timeout)
         self.sequence.append(cmd)
 
     def update(self):
-        fvs = {}
         for _ in range(1, len(self.fvs)):
             self.fvs[random.choice(list(self.fvs.keys()))] = util.RAND(30)
         tmpl = super().update()
-        cmd = formatter.fmt_mstring(tmpl, key=self.key, fvs=fvs, timeout=self.timeout)
+        cmd = formatter.fmt_mstring(tmpl, key=self.key, fvs=self.fvs, timeout=self.timeout)
         self.sequence.append(cmd)
 
     def require(self):
